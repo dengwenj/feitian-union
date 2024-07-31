4dab6e3a96e551211dc176682f8f7c8b55fef5bb
朴睦 <927561237@qq.com>
2024-07-31 15:33:45 +0800

    淘口令界面显示

--- app/src/main/java/vip/dengwj/feitian_union/ui/activity/TicketActivity.java
+++ app/src/main/java/vip/dengwj/feitian_union/ui/activity/TicketActivity.java
@@ -1,16 +1,20 @@
 package vip.dengwj.feitian_union.ui.activity;
 
+import com.bumptech.glide.Glide;
+
 import vip.dengwj.feitian_union.base.BaseActivity;
 import vip.dengwj.feitian_union.databinding.ActivityTicketBinding;
 import vip.dengwj.feitian_union.model.domain.Ticket;
 import vip.dengwj.feitian_union.presenter.TicketPresenter;
 import vip.dengwj.feitian_union.utils.LogUtils;
 import vip.dengwj.feitian_union.utils.PresenterManager;
+import vip.dengwj.feitian_union.utils.UrlUtils;
 import vip.dengwj.feitian_union.view.TicketCallback;
 
 public class TicketActivity extends BaseActivity<ActivityTicketBinding> implements TicketCallback {
 
     private TicketPresenter ticketPresenter;
+    private ActivityTicketBinding ticketBinding;
 
     @Override
     public void initPresenter() {
@@ -31,13 +35,21 @@ public class TicketActivity extends BaseActivity<ActivityTicketBinding> implemen
 
     @Override
     public ActivityTicketBinding getBinding() {
-        return ActivityTicketBinding.inflate(getLayoutInflater());
+        ticketBinding = ActivityTicketBinding.inflate(getLayoutInflater());
+        return ticketBinding;
+    }
+
+    @Override
+    public void initEvent() {
+        ticketBinding.back.setOnClickListener(v -> finish());
     }
 
     @Override
     public void onTicketLoaded(String cover, Ticket result) {
-        LogUtils.d(TicketActivity.class, "cover -> " + cover);
-        LogUtils.d(TicketActivity.class, "result -> " + result);
+        String model = result.getData().getTbk_tpwd_create_response().getData().getModel();
+        String[] split = model.split("￥");
+        Glide.with(this).load(UrlUtils.getCoverPath(cover, 300)).into(ticketBinding.ticketImg);
+        ticketBinding.ticketCode.setText(String.format("￥" + split[1] + "￥"));
     }
 
     @Override
