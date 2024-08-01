5c5d17283ba2cef7496253fda049c5b0488348ae
朴睦 <927561237@qq.com>
2024-08-01 15:50:34 +0800

    精选左侧界面展示

---
+++ app/src/main/java/vip/dengwj/feitian_union/ui/adapter/SelectedLeftAdapter.java
@@ -0,0 +1,88 @@
+package vip.dengwj.feitian_union.ui.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vip.dengwj.feitian_union.R;
+import vip.dengwj.feitian_union.databinding.ItemSelectedLeftBinding;
+import vip.dengwj.feitian_union.model.domain.SelectedCategory;
+
+public class SelectedLeftAdapter extends RecyclerView.Adapter<SelectedLeftAdapter.Holder> {
+    private final List<SelectedCategory.DataBean> list = new ArrayList<>();
+    private ItemSelectedLeftBinding selectedLeftBinding;
+    private int currentPosition = 0;
+
+    private OnCategoryLeftClickListener onCategoryLeftClickListener;
+
+    @NonNull
+    @Override
+    public SelectedLeftAdapter.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        selectedLeftBinding = ItemSelectedLeftBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new Holder(selectedLeftBinding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SelectedLeftAdapter.Holder holder, int position) {
+        SelectedCategory.DataBean dataBean = list.get(position);
+        holder.setData(dataBean, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public void setData(List<SelectedCategory.DataBean> categoryList) {
+        list.clear();
+        list.addAll(categoryList);
+        notifyDataSetChanged();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.category_title);
+            textView.setOnClickListener(v -> {
+                currentPosition = (Integer) v.getTag();
+                notifyDataSetChanged();
+                if (onCategoryLeftClickListener != null) {
+                    onCategoryLeftClickListener.onCategoryLeftClick();
+                }
+            });
+        }
+
+        public void setData(SelectedCategory.DataBean dataBean, int position) {
+            textView.setText(dataBean.getMaterialName());
+            // 做个标识
+            textView.setTag(position);
+
+            if (position == currentPosition) {
+                textView.setBackgroundColor(
+                        textView.getResources().getColor(R.color.category_left)
+                );
+            } else {
+                textView.setBackgroundColor(
+                        textView.getResources().getColor(R.color.white)
+                );
+            }
+        }
+    }
+
+    public void setOnCategoryLeftClickListener(OnCategoryLeftClickListener listener) {
+        onCategoryLeftClickListener = listener;
+    }
+
+    public interface OnCategoryLeftClickListener {
+        void onCategoryLeftClick();
+    }
+}
--- app/src/main/java/vip/dengwj/feitian_union/ui/fragment/SelectedFragment.java
+++ app/src/main/java/vip/dengwj/feitian_union/ui/fragment/SelectedFragment.java
@@ -2,13 +2,17 @@ package vip.dengwj.feitian_union.ui.fragment;
 
 import android.view.View;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import java.util.List;
 
 import vip.dengwj.feitian_union.R;
 import vip.dengwj.feitian_union.base.BaseFragment;
+import vip.dengwj.feitian_union.databinding.FragmentSelectedBinding;
 import vip.dengwj.feitian_union.model.domain.HomePagerContent;
 import vip.dengwj.feitian_union.model.domain.SelectedCategory;
 import vip.dengwj.feitian_union.presenter.SelectedPagePresenter;
+import vip.dengwj.feitian_union.ui.adapter.SelectedLeftAdapter;
 import vip.dengwj.feitian_union.utils.LogUtils;
 import vip.dengwj.feitian_union.utils.PresenterManager;
 import vip.dengwj.feitian_union.view.SelectedCallback;
@@ -16,6 +20,8 @@ import vip.dengwj.feitian_union.view.SelectedCallback;
 public class SelectedFragment extends BaseFragment implements SelectedCallback {
 
     private SelectedPagePresenter selectedPagePresenter;
+    private FragmentSelectedBinding selectedBinding;
+    private SelectedLeftAdapter selectedLeftAdapter;
 
     @Override
     public int loadRootViewId() {
@@ -25,6 +31,21 @@ public class SelectedFragment extends BaseFragment implements SelectedCallback {
     @Override
     public void initView(View rootView) {
         setupState(State.SUCCESS);
+        selectedBinding = FragmentSelectedBinding.bind(rootView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        selectedBinding.leftCategory.setLayoutManager(layoutManager);
+        selectedLeftAdapter = new SelectedLeftAdapter();
+        selectedBinding.leftCategory.setAdapter(selectedLeftAdapter);
+    }
+
+    @Override
+    public void initListener() {
+        selectedLeftAdapter.setOnCategoryLeftClickListener(new SelectedLeftAdapter.OnCategoryLeftClickListener() {
+            @Override
+            public void onCategoryLeftClick() {
+                LogUtils.d(SelectedFragment.class, "SelectedFragment");
+            }
+        });
     }
 
     @Override
@@ -42,7 +63,7 @@ public class SelectedFragment extends BaseFragment implements SelectedCallback {
 
     @Override
     public void categoryLoaded(List<SelectedCategory.DataBean> categoryList) {
-        LogUtils.d(SelectedFragment.class, "categoryList -> " + categoryList);
+        selectedLeftAdapter.setData(categoryList);
     }
 
     @Override
