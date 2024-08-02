2950406966e1587d63be7a1ea5525051fbb416d8
朴睦 <927561237@qq.com>
2024-08-02 15:55:58 +0800

    特惠宝贝 UI 展示

--- app/src/main/java/vip/dengwj/feitian_union/ui/adapter/RedPacketAdapter.java
+++ app/src/main/java/vip/dengwj/feitian_union/ui/adapter/RedPacketAdapter.java
@@ -1,12 +1,16 @@
 package vip.dengwj.feitian_union.ui.adapter;
 
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +20,7 @@ import vip.dengwj.feitian_union.databinding.RedPacketItemBinding;
 import vip.dengwj.feitian_union.model.domain.HomePagerContent;
 import vip.dengwj.feitian_union.ui.fragment.RedPackedFragment;
 import vip.dengwj.feitian_union.utils.LogUtils;
+import vip.dengwj.feitian_union.utils.UrlUtils;
 
 public class RedPacketAdapter extends RecyclerView.Adapter<RedPacketAdapter.Holder> {
     private final List<HomePagerContent.DataBean.ListBean> list = new ArrayList<>();
@@ -29,9 +34,21 @@ public class RedPacketAdapter extends RecyclerView.Adapter<RedPacketAdapter.Hold
 
     @Override
     public void onBindViewHolder(@NonNull RedPacketAdapter.Holder holder, int position) {
+        View itemView = holder.itemView;
         HomePagerContent.DataBean.ListBean listBean = list.get(position);
-        TextView textView = holder.itemView.findViewById(R.id.test);
-        textView.setText(listBean.getTitle());
+
+        ImageView imageView = itemView.findViewById(R.id.img);
+        TextView title = itemView.findViewById(R.id.title);
+        TextView oldPrice = itemView.findViewById(R.id.old_price);
+        TextView newPrice = itemView.findViewById(R.id.new_price);
+
+        Glide.with(itemView.getContext()).load(UrlUtils.getCoverPath(listBean.getCover(), 200)).into(imageView);
+        title.setText(listBean.getTitle());
+        String oldP = "￥" + String.format("%.2f", Float.parseFloat(listBean.getJustPrice()));
+        oldPrice.setText(oldP);
+        oldPrice.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        String newP = "券后价：" + String.format("%.2f", Float.parseFloat(listBean.getZkFinalPrice()));
+        newPrice.setText(newP);
     }
 
     @Override
