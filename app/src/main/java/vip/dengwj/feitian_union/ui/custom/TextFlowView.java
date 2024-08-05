7be32b3ee7aad1bdbf547d89cd14620cfb60ce60
朴睦 <927561237@qq.com>
2024-08-05 17:43:39 +0800

    TextFlowView set 数据

---
+++ app/src/main/java/vip/dengwj/feitian_union/ui/custom/TextFlowView.java
@@ -0,0 +1,56 @@
+package vip.dengwj.feitian_union.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import vip.dengwj.feitian_union.R;
+import vip.dengwj.feitian_union.utils.LogUtils;
+
+public class TextFlowView extends ViewGroup {
+    public TextFlowView(Context context) {
+        this(context, null);
+    }
+
+    public TextFlowView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TextFlowView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    /**
+     * 设置数据
+     */
+    public void setTextList(List<String> list) {
+        for (String val : list) {
+            TextView item = (TextView) LayoutInflater
+                    .from(getContext())
+                    .inflate(R.layout.flow_text, this, false);
+            item.setText(val);
+            // 添加孩子
+            addView(item);
+        }
+    }
+
+    /**
+     * 测量
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        LogUtils.d(TextFlowView.class, "onMeasure -> " + getChildCount());
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        // 布局，摆放孩子
+        LogUtils.d(TextFlowView.class, "onLayout -> " + getChildCount());
+    }
+}
